f85579af8616b91cf1a9dd4985834038cd4909a4
Kalamojo <kalabiprof@gmail.com>
2022-09-03 16:14:39 -0500

    largest items

--- traverse.py
+++ traverse.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify, render_template
 import cohere
 import math
+from heapq import nsmallest
 
 test = Flask(__name__)
 
 #@test.before_first_request
 #def do_something_only_onnce():
 co = cohere.Client('0Qp52FnTMwc3dhwWafuGWw8yOqdyy1bKK0usvqxD')
+options = ["When are you open?", "When do you close?", "What are the hours?", "Are you open on weekends?", "Are you available on holidays?", "How much is a burger?", "What\'s the price of a meal?", "How much for a few burgers?", "Do you have a vegan option?", "Do you have vegetarian?", "Do you serve non-meat alternatives?", "Do you have milkshakes?", "Milkshake", "Do you have desert?", "Can I bring my child?", "Are you kid friendly?", "Do you have booster seats?", "Do you do delivery?", "Is there takeout?", "Do you deliver?", "Can I have it delivered?", "Can you bring it to me?", "Do you have space for a party?", "Can you accommodate large groups?", "Can I book a party here?"]
 response = co.embed(
     model='large',
-    texts=["When are you open?", "When do you close?", "What are the hours?", "Are you open on weekends?", "Are you available on holidays?", "How much is a burger?", "What\'s the price of a meal?", "How much for a few burgers?", "Do you have a vegan option?", "Do you have vegetarian?", "Do you serve non-meat alternatives?", "Do you have milkshakes?", "Milkshake", "Do you have desert?", "Can I bring my child?", "Are you kid friendly?", "Do you have booster seats?", "Do you do delivery?", "Is there takeout?", "Do you deliver?", "Can I have it delivered?", "Can you bring it to me?", "Do you have space for a party?", "Can you accommodate large groups?", "Can I book a party here?"])
+    texts=options)
     
 def co_sim(arr1, arr2, size):
     tempT = 0
@@ -31,11 +33,12 @@ def actualStuff():
     search = query_parameters.get('search')
     print(search)
     emb = co.embed(model='large', texts=[search]).embeddings[0]
-    cosine_sim = []
-    for phrase in response.embeddings:
-        cosine_sim.append(co_sim(emb, phrase, 4096))
+    cosine_sim = {}
+    for i in range(len(options)):
+        phrase = response.embeddings[i]
+        cosine_sim[co_sim(emb, phrase, 4096)] = options[i]
 	# call the function on the search and then return results
-    return jsonify(cosine_sim)
+    return jsonify(nsmallest(5, cosine_sim.items()))
 
 if __name__ == "__main__":
 	test.run(debug=True)
